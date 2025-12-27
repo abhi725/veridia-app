419e0df3f3a530f6561c5df0bcac2e8c402259c2
ABHISHEK AMBAD <50826824+abhi725@users.noreply.github.com>
2025-12-28 01:21:42 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',  // <--- THIS IS THE MAGIC LINE
+  // any other config you had...
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
